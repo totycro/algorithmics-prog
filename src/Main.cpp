e78c9c60346c3a8dae012ced252258ea54a6df08
Christof Schmidt <alg0627958@eowyn.ads.tuwien.ac.at>
2013-01-15 18:59:24 +0100

    added logging

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,7 @@
 #define __MAIN__CPP__
 
 #include <iostream>
+#include <fstream>
 #include "Tools.h"
 #include "Instance.h"
 #include "kMST_ILP.h"
@@ -10,8 +11,8 @@ using namespace std;
 
 void usage()
 {
-	cout << "USAGE:\t<program> -f filename -m model [-k <nodes to connect>]\n";
-	cout << "EXAMPLE:\t" << "./kmst -f data/g01.dat -m scf -k 5\n\n";
+	cout << "USAGE:\t<program> -f filename -m model [-k <nodes to connect> -l <logfile>]\n";
+	cout << "EXAMPLE:\t" << "./kmst -f data/g01.dat -m scf -k 5 -l log.txt\n\n";
 	exit( 1 );
 } // usage
 
@@ -23,7 +24,10 @@ int main( int argc, char *argv[] )
 	string file( "data/g01.dat" );
 	string model_type( "flow" );
 	int k = 5;
-	while( (opt = getopt( argc, argv, "f:m:k:" )) != EOF) {
+	bool doLogging = false;
+	string logFilename("");
+	int rounds = 1;
+	while( (opt = getopt( argc, argv, "f:m:k:l:r:" )) != EOF) {
 		switch( opt ) {
 			case 'f': // instance file
 				file = optarg;
@@ -34,6 +38,13 @@ int main( int argc, char *argv[] )
 			case 'k': // nodes to connect
 				k = atoi( optarg );
 				break;
+			case 'l': // logfile
+				logFilename = optarg;
+				doLogging = true;
+				break;	
+			case 'r': // rounds of executions
+				rounds = atoi( optarg );
+				break;	
 			default:
 				usage();
 				break;
@@ -42,8 +53,36 @@ int main( int argc, char *argv[] )
 	// read instance
 	Instance instance( file );
 	// solve instance
-	kMST_ILP ilp( instance, model_type, k );
-	ilp.solve();
+	double cpuTimeSum = 0;
+	double objectiveValue;
+	int nodes;
+	for (int round=0; round<rounds; round++) {
+		kMST_ILP ilp( instance, model_type, k );
+		ilp.solve();
+		cpuTimeSum += ilp.getcpuTime();
+		objectiveValue = ilp.getObjectiveValue();
+		nodes = ilp.getNodes();
+	}
+
+	// log results
+	if (doLogging) {
+		bool exists = false;
+		ifstream logIn(logFilename.c_str());
+		if (logIn) {
+			exists = true;
+		}
+		logIn.close();
+
+		ofstream log;
+		log.open(logFilename.c_str(),ios::ate | ios::app);
+ 		if (!exists) {
+			log <<"Filename\tModel\tNodes\tCost\tB&B N\tCPUTime\r\n";
+		}
+		log <<file <<"\t"<< model_type <<"\t"<< k <<"\t"<< 
+			objectiveValue <<"\t"<< nodes
+			<<"\t"<< cpuTimeSum / rounds<<"\r\n";
+ 		log.close();
+	}
 
 	return 0;
 } // main
